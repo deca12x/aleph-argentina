b8912c1100e03ad4bb1b0c5d6b0321f6aba48ff5
Giulio de Cadilhac <g.decadilhac@gmail.com>
2025-03-22 15:44:22 -0300

    Feat: add Mantle & zkSync clans

--- lib/data.ts
+++ lib/data.ts
@@ -3,5 +3,7 @@ import { Clan } from "@/lib/types";
 export const clans: Clan[] = [
   { id: "clan1", name: "Crecimiento" },
   { id: "clan2", name: "Urbe" },
-  { id: "clan3", name: "Aleph" },
+  { id: "clan3", name: "zkSync" },
+  { id: "clan4", name: "Mantle" },
+  { id: "clan5", name: "Aleph" },
 ];
