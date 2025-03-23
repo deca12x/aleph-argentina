1d59f8478ab3f5e7476cd88cca725abcac89b138
Joaquin <jfarfantorres@gmail.com>
2025-03-22 21:02:17 -0300

    created missing lib components

--- lib/data.ts
+++
@@ -1,8 +0,0 @@
-import { Clan } from "@/lib/types";
-
-export const clans: Clan[] = [
-  { id: "clan1", name: "Crecimiento" },
-  { id: "clan2", name: "Urbe" },
-  { id: "clan3", name: "Aleph" },
-  { id: "clan4", name: "Mantle" },
-];
